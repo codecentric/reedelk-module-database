044dad07814525ea8ad98252656d1ff30062c14b
lorenzo <lorenzo@lorenzo.info.com>
2020-02-06 14:48:22 +0100

    code

---
+++ src/main/java/com/reedelk/database/component/DDLExecute.java
@@ -0,0 +1,102 @@
+package com.reedelk.database.component;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.reedelk.database.commons.DataSourceService;
+import com.reedelk.database.commons.DatabaseUtils;
+import com.reedelk.database.commons.QueryStatementTemplate;
+import com.reedelk.database.configuration.ConnectionConfiguration;
+import com.reedelk.runtime.api.annotation.ESBComponent;
+import com.reedelk.runtime.api.annotation.Property;
+import com.reedelk.runtime.api.annotation.TabPlacementTop;
+import com.reedelk.runtime.api.component.ProcessorSync;
+import com.reedelk.runtime.api.exception.ESBException;
+import com.reedelk.runtime.api.flow.FlowContext;
+import com.reedelk.runtime.api.message.Message;
+import com.reedelk.runtime.api.message.MessageBuilder;
+import com.reedelk.runtime.api.script.ScriptEngineService;
+import com.reedelk.runtime.api.script.dynamicmap.DynamicObjectMap;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ServiceScope;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Map;
+
+import static com.reedelk.runtime.api.commons.ConfigurationPreconditions.requireNotBlank;
+
+@ESBComponent("DDL Execute")
+@Component(service = DDLExecute.class, scope = ServiceScope.PROTOTYPE)
+public class DDLExecute implements ProcessorSync {
+
+    @Property("Connection")
+    private ConnectionConfiguration connectionConfiguration;
+
+    @Property("DDL Definition")
+    private String query;
+
+    @Property("Query Variables Mappings")
+    @TabPlacementTop
+    private DynamicObjectMap parametersMapping = DynamicObjectMap.empty();
+
+    @Reference
+    private DataSourceService dataSourceService;
+    @Reference
+    private ScriptEngineService scriptEngine;
+
+    private QueryStatementTemplate queryStatement;
+
+    private ComboPooledDataSource dataSource;
+
+    @Override
+    public void initialize() {
+        requireNotBlank(DDLExecute.class, query, "DDL definition is not defined");
+        dataSource = dataSourceService.getDataSource(this, connectionConfiguration);
+        queryStatement = new QueryStatementTemplate(query);
+    }
+
+    @Override
+    public Message apply(FlowContext flowContext, Message message) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+
+            Map<String, Object> evaluatedMap = scriptEngine.evaluate(parametersMapping, flowContext, message);
+            String realQuery = queryStatement.replace(evaluatedMap);
+
+            int rowCount = statement.executeUpdate(realQuery);
+
+            return MessageBuilder.get().withJavaObject(rowCount).build();
+
+        } catch (Throwable exception) {
+            throw new ESBException(exception);
+        } finally {
+            DatabaseUtils.closeSilently(resultSet);
+            DatabaseUtils.closeSilently(statement);
+            DatabaseUtils.closeSilently(connection);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        this.dataSourceService.dispose(this, connectionConfiguration);
+        this.dataSource = null;
+        this.queryStatement = null;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public void setConnectionConfiguration(ConnectionConfiguration connectionConfiguration) {
+        this.connectionConfiguration = connectionConfiguration;
+    }
+
+    public void setParametersMapping(DynamicObjectMap parametersMapping) {
+        this.parametersMapping = parametersMapping;
+    }
+}
